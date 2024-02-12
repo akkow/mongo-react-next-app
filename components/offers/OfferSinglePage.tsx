e92b39908e426c382967e90cd53da9736234d02c
akkow <birdxxteam@gmail.com>
2024-02-13 00:11:27 +0200

    alerts

--- components/offers/OfferSinglePage.tsx
+++ components/offers/OfferSinglePage.tsx
@@ -17,6 +17,8 @@ export function OfferSinglePage(props: IProps) {
     const { data: session, update } = useSession()
 
     const [profileOfferData, setProfileOfferData] = useState<UserDto>({} as UserDto)
+    const [click, setClick] = useState(true)
+    const [fade, setFade] = useState(true)
     
     // useEffect(() => {
     //     let savedOffers = []
@@ -27,16 +29,28 @@ export function OfferSinglePage(props: IProps) {
 
     useEffect(() => {
         fetch(`/api/users/${session.user._id}`).then((res) => res.json()).then((data) => setProfileOfferData(data))
-    }, [offer])
+    }, [])
 
     const saveOffer = () => {
         let savedOffers: any = []
         if(profileOfferData?.savedOffers?.length) savedOffers = profileOfferData.savedOffers
         if(!savedOffers.includes(offer._id)) { 
             savedOffers.push(offer._id)
+            setClick(true)
+            document.getElementById('text-of-alert').innerHTML = 'Išsaugotas skelbimas!'
+            document.getElementById('alert-success').style.display = 'inherit'
+            setTimeout(() => {
+                document.getElementById('alert-success').style.display = 'none'
+            }, 2000);
         } else {
             let index = savedOffers.indexOf(offer._id)
             savedOffers.splice(index, 1)
+            setClick(false)
+            document.getElementById('text-of-alert').innerHTML = 'Pašalintas skelbimas iš išsaugotų sąrašo!'
+            document.getElementById('alert-success').style.display = 'inherit'
+            setTimeout(() => {
+                document.getElementById('alert-success').style.display = 'none'
+            }, 2000);
         }
         profileOfferData.savedOffers = savedOffers
         fetch(`/api/users/${profileOfferData._id}`, {
@@ -46,8 +60,28 @@ export function OfferSinglePage(props: IProps) {
         })
         .then((res) => {res.json()})
     }
+
+    useEffect(() => {
+        if(profileOfferData.savedOffers?.includes(offer._id)) {
+            setClick(true)
+        }
+        else {
+            setClick(false)
+        }
+        if(click) { 
+            document.getElementById('book-mark-svg').style.fill = 'rgb(90, 255, 184)'
+        }
+        else {
+            document.getElementById('book-mark-svg').style.fill = 'white'
+        }
+    })
+
     return (
         <>
+            <div id='alert-success' role="alert" className={`hidden alert alert-success absolute left-[40%] top-10 w-96 transition-all duration-1000 ${fade ? "opacity-100" : "opacity-0"}`}>
+                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 12l2 2 4-4m6 2a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+                <span className="font-bold" id='text-of-alert'></span>
+            </div>
             <div>
                 <div className="relative flex flex-col items-center rounded-lg shadow-xl border w-[80%] mt-10 mx-auto">
                     <div className="w-[90%] py-4">
